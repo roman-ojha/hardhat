89c198672f251fae1c50d70b1656dd841fb4a8ac
Roman-Ojha <razzroman98@gmail.com>
2022-09-07 08:45:12 +0545

    docs: tutorial: Hardhat installation

---
+++ Tutorial/01_Main_Tutorial/02_Installation/Note.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
+
+/*
+ *) Installation:
+    -> npm init -y
+    -> yarn add -D hardhat
+        -> npm i --save-dev hardhat
+    -> npx hardhat
+        ->(to initialize hardhat project)
+    -> yarn add -D @nomiclabs/hardhat-ethers ethers @nomiclabs/hardhat-waffle ethereum-waffle chai
+        ->(library for testing contract)
+    -> after that add some code in 'hardhat.config.js'
+ */
---
+++ Tutorial/01_Main_Tutorial/02_Installation/hardhat.config.js
@@ -0,0 +1,7 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+
+require("@nomiclabs/hardhat-waffle");
+module.exports = {
+  // which solidity version you want to use for this project
+  solidity: "0.8.9",
+};
