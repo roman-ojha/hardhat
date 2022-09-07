0626eac070802a791be59c2e96d3dd89852371ae
Roman-Ojha <razzroman98@gmail.com>
2022-09-07 08:31:13 +0545

    docs: Hardhat introduction

---
+++ Tutorial/01_Main_Tutorial/01_Intorudction/Introduction.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
+
+/*
+ *) Hardhat
+        -> It is also a development environment like truffle
+        -> through which we can do test, deploy, compile, debug you ethereum software
+        -> It is written in JavaScript Language
+*) Hardhat vs Truffle:
+    => Hardhat:
+        -> Faster migration process
+        -> Less gas consumption during migration
+        -> Debugging is easy
+    => Truffle:
+        -> Poor migration process
+        -> More gas consumption during migration
+        -> Not good as compared to Hardhat
+ */
