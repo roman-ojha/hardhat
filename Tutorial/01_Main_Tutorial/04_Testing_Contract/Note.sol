c0fe52d0efe34429871a0206633114bbe1eb2b3b
Roman-Ojha <razzroman98@gmail.com>
2022-09-07 11:24:58 +0545

    feat: tested contract using Hooks

--- Tutorial/01_Main_Tutorial/04_Testing_Contract/Note.sol
+++ Tutorial/01_Main_Tutorial/04_Testing_Contract/Note.sol
@@ -3,6 +3,7 @@ pragma solidity >=0.6.0 <0.9.0;
 
 /*
     -> https://www.chaijs.com/
+    -> https://ethereum-waffle.readthedocs.io/en/latest/index.html
     => we will use Mocha framework, Chai library, ethers to test the contract
     => we will create a new file 'token.js' inside 'test'
     -> npx hardhat test
--- Tutorial/01_Main_Tutorial/04_Testing_Contract/test/token.js
+++ Tutorial/01_Main_Tutorial/04_Testing_Contract/test/token.js
@@ -9,7 +9,7 @@ describe("Token contract", function () {
     // getSigners() is a object through which we can access account, balance of that account
     // 'owner' contain the address through which we will deploy the smart contract
 
-    console.log("Signers: ", owner);
+    // console.log("Signers: ", owner);
 
     // Create instance of contract
     const Token = await ethers.getContractFactory("Token");
@@ -20,8 +20,8 @@ describe("Token contract", function () {
     // Now accessing the contract function
     // checking balance of given address
     const ownerBalance = await hardhatToken.checkBalance(owner.address);
-    console.log("Owner Address: ", owner.address);
-    console.log("Owner Balance: ", ownerBalance);
+    // console.log("Owner Address: ", owner.address);
+    // console.log("Owner Balance: ", ownerBalance);
 
     // Now we will use chai library through which we can access expect
     expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
@@ -59,5 +59,93 @@ describe("Token contract", function () {
     expect(await hardhatToken.checkBalance(owner.address)).to.equal(
       10000 - transferAmountToAddress1
     );
+  });
+});
+
+// In above described test we have repeated code multiple times in different test which is not a good thing to do so we will use Hooks which is provided by Mocha framework
+
+describe("Token Contract Using Hooks", function () {
+  // now here we will create the multiple variable that we will use in test
+  let Token;
+  let hardhatToken;
+  let owner;
+  let address1;
+  let address2;
+  let addresses;
+
+  beforeEach(async function () {
+    // beforeEach will run every time when each test run
+    Token = await ethers.getContractFactory("Token");
+    [owner, address1, address2, ...addresses] = await ethers.getSigners();
+    hardhatToken = await Token.deploy();
+  });
+
+  describe("Deployment", function () {
+    // we will add test here for Deployment
+    it("Should set the right owner", async function () {
+      expect(await hardhatToken.owner()).to.equal(owner.address);
+    });
+
+    it("Deployment should assign the total supply of tokens to the owner", async function () {
+      const ownerBalance = await hardhatToken.checkBalance(owner.address);
+      expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    });
+  });
+
+  describe("Transactions", function () {
+    // Write test for Transaction
+    it("Should transfer tokens between accounts", async function () {
+      // Transfer 10 token from owner to address1
+      const transferAmountToAddress1 = 10;
+      await hardhatToken.transfer(address1.address, transferAmountToAddress1);
+      expect(await hardhatToken.checkBalance(address1.address)).to.equal(
+        transferAmountToAddress1
+      );
+
+      // Transfer 5 token from address1 to address2
+      const transferAmountToAddress2 = 5;
+      await hardhatToken
+        .connect(address1)
+        .transfer(address2.address, transferAmountToAddress2);
+      expect(await hardhatToken.checkBalance(address2.address)).to.equal(
+        transferAmountToAddress2
+      );
+      expect(await hardhatToken.checkBalance(owner.address)).to.equal(
+        10000 - transferAmountToAddress1
+      );
+    });
+
+    it("Should fail if sender doesn't have enough tokens", async function () {
+      const initialOwnerBalance = await hardhatToken.checkBalance(
+        owner.address
+      );
+      // now here while transferring token from address1 to owner contract should revert back with String "Not enough tokens"
+      // https://ethereum-waffle.readthedocs.io/en/latest/matchers.html#revert-with-message
+      await expect(
+        hardhatToken.connect(address1).transfer(owner.address, 1)
+      ).to.be.revertedWith("Not enough tokens");
+
+      // now we will check does the balance of owner is equal to initial balance of owner
+      expect(await hardhatToken.checkBalance(owner.address)).to.equal(
+        initialOwnerBalance
+      );
+    });
+
+    it("Should update balance after transfers", async function () {
+      const initialOwnerBalance = await hardhatToken.checkBalance(
+        owner.address
+      );
+      await hardhatToken.transfer(address1.address, 5);
+      await hardhatToken.transfer(address2.address, 10);
+
+      const finalOwnerBalance = await hardhatToken.checkBalance(owner.address);
+      expect(finalOwnerBalance).to.equal(initialOwnerBalance - 5 - 10);
+
+      const addr1Balance = await hardhatToken.checkBalance(address1.address);
+      expect(addr1Balance).to.equal(5);
+
+      const addr2Balance = await hardhatToken.checkBalance(address2.address);
+      expect(addr2Balance).to.equal(10);
+    });
   });
 });
