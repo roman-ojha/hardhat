ccf94fad3727b83a57526822fc8db4831af5e617
Roman-Ojha <razzroman98@gmail.com>
2022-09-07 11:41:05 +0545

    docs: Degugging sol code using hardhat

--- Tutorial/01_Main_Tutorial/05_Debugging/Note.sol
+++ Tutorial/01_Main_Tutorial/05_Debugging/Note.sol
@@ -2,10 +2,8 @@
 pragma solidity >=0.6.0 <0.9.0;
 
 /*
-    -> https://www.chaijs.com/
-    -> https://ethereum-waffle.readthedocs.io/en/latest/index.html
-    => we will use Mocha framework, Chai library, ethers to test the contract
-    => we will create a new file 'token.js' inside 'test'
-    -> npx hardhat test
-        -> (to run test)
+    => In hardhat we can debug solidity code
+    => for that we have to import hardhat console.log into solidity file 'contracts/Token.sol'
+    => so, after adding console.log() while testing it we will be able to see those console log statement
+        => npx hardhat test
  */
--- Tutorial/01_Main_Tutorial/05_Debugging/contracts/Token.sol
+++ Tutorial/01_Main_Tutorial/05_Debugging/contracts/Token.sol
@@ -2,6 +2,9 @@
 
 pragma solidity >=0.6.0 <0.9.0;
 
+// for debugging code you have to import this:
+import "hardhat/console.sol";
+
 contract Token {
     string public name = "HardHat Token";
     string public symbol = "HHT";
@@ -15,6 +18,10 @@ contract Token {
     }
 
     function transfer(address to, uint256 amount) external {
+        // now to console log we have to do this:
+        console.log("**Sender balance is %s tokens", balances[msg.sender]);
+        console.log("**Sender is sending %s tokens to %s address", amount, to);
+
         require(balances[msg.sender] >= amount, "Not enough tokens");
         balances[msg.sender] -= amount;
         balances[to] += amount;
